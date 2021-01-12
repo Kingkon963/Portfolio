e0898b63789641c897b7e3abf7eda4c92766ac83
Naim <190204081@aust.edu>
2021-01-12 14:37:16 +0600

    user-select fixed to none & skill indicator added

--- pages/Skills.js
+++ pages/Skills.js
@@ -1,11 +1,71 @@
 import Layout from "../components/Layout";
 
+
+function SkillIndicator({level}){
+    let color = ""
+    switch(level){
+        case "Advance":
+            color = "#EC1AD0"
+            break;
+        case "Intermediate":
+            color = "#1A83EC"
+            break;
+        case "Basics":
+            color = "#1AEC67"
+            break;
+        default:
+            color = "white"
+    }
+
+    return(
+    <>
+        <div className="indicator" ></div>
+        <style jsx>{`
+            .indicator{
+                min-width: 15px;
+                min-height: 15px;
+                border-radius: 50%;
+                background-color: ${color};
+                margin: 0px;
+            }
+        `}</style>
+    </>
+    )
+}
+
 export default function Skills(){
     return(
     <>
         <Layout setActiveLink="Skills">
-            <h1>Skills</h1>
+            <ul>
+                <SkillIndicator level="Advance"/>
+                <li>Advance</li>
+
+                <SkillIndicator level="Intermediate"/>
+                <li>Intermediate</li>
+
+                <SkillIndicator level="Basics"/>
+                <li>Basics</li>
+            </ul>
         </Layout>
+
+        <style jsx>
+        {`
+            ul{
+                //border: 1px solid white;
+                list-style: none;
+                display: flex;
+                align-items: center;
+                padding: 0px 1.5em;
+            }    
+            li{
+                color: #B7D6EC;
+                opacity: 86%;
+                margin-left: .3em;
+                margin-right: 1.2em;
+            }
+        `}
+        </style>
     </>
     )
 }
