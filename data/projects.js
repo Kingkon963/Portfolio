924b1c30c13b93032971e2ad01655e1c9d7961d2
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2021-06-17 16:44:24 +0600

    portfolio & element animation updated

--- data/projects.js
+++ data/projects.js
@@ -1,22 +1,15 @@
 const projects = [
-    {
-        name: 'myKashBack',
-        thumbnail: '/images/projects/myKashBack.png',
-        width: 300,
-        height: 300,
-        status: "Completed",
-        technology: "Pure HTML CSS & JS",
-        description: "This Landing Page was for a UK based startup company who offers their client cashback discounts on using their App for purchasing from their various partner stores/restaurants."
-    },
-    {
-        name: 'Accounting Farm',
-        thumbnail: '/images/projects/accountant.jpg',
-        width: 300,
-        height: 300,
-        status: "Developing",
-        technology: "Nextjs",
-        description: "This Landing Page was for a UK based startup company who offers their client cashback discounts on using their App for purchasing from their various partner stores/restaurants."
-    },
-]; 
+  {
+    name: "myKashBack",
+    thumbnail: "/images/projects/myKashBack.png",
+    width: 300,
+    height: 300,
+    status: "Completed",
+    technology: "Pure HTML CSS & JS",
+    description: `This Landing Page was for a UK based startup company who offers their client 
+      cashback discounts for using their App on purchasing from their various 
+      partner stores/restaurants.`,
+  },
+];
 
 export default projects;
--- pages/Portfolio/[name].js
+++ pages/Portfolio/[name].js
@@ -1,125 +1,72 @@
-import {useState, useEffect} from 'react'
-import { useRouter } from 'next/router'
-import Head from 'next/head'
-import Image from 'next/image'
-import projects from '../../data/projects'
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Image from "next/image";
+import projects from "../../data/projects";
 
-export default function ProjectDetail({props}){
-    const [project, setProject] = useState()
-    
-    const router = useRouter()
-    const {name, id} = router.query
+import styles from "../../styles/PortfolioDetails.module.scss";
 
-    useEffect(() => {
-        if(id){
-            setProject(projects[id])
-        }    
-    }, project)
-        
+export default function ProjectDetail({ props }) {
+  const [project, setProject] = useState();
 
-    return(
+  const router = useRouter();
+  const { name, id } = router.query;
+
+  useEffect(() => {
+    if (id) {
+      setProject(projects[id]);
+    }
+  }, project);
+
+  return (
     <>
-        <Head>
-            <title>{name}</title>
-            <meta name="theme-color" content="#162A31" />
-        </Head>
-        {project ? 
-        <div className="container">
-            <img src="/backArrow.svg" alt="Back button" className="backArrow" onClick={() => router.back()}/>
+      <Head>
+        <title>{name}</title>
+        <meta name="theme-color" content="#162A31" />
+      </Head>
+      {project ? (
+        <>
+          <div className={styles.container}>
+            <img
+              src="/backArrow.svg"
+              alt="Back button"
+              className={styles.backArrow}
+              onClick={() => router.back()}
+            />
 
-            <div className="banner">
-                <Image src={project.thumbnail} width={project.width} height={project.height} loading="eager"/>
+            <div className={styles.banner}>
+              <Image
+                src={project.thumbnail}
+                width={project.width}
+                height={project.height}
+                loading="eager"
+              />
             </div>
 
-            <div className="content">
-                <div className="title">
-                    <h1>{project.name}</h1>
-                </div>
+            <div className={styles.content}>
+              <div className={styles.title}>
+                <h1>{project.name}</h1>
+              </div>
 
-                <div className="info">
-                    <span>Status</span>
-                    <span className="info-value">{project.status}</span>
+              <div className={styles.info}>
+                <span>Status</span>
+                <span className={styles.infoValue}>{project.status}</span>
 
-                    <span>Technology</span>
-                    <span className="info-value">{project.technology}</span>
-                </div>
+                <span>Technology</span>
+                <span className={styles.infoValue}>{project.technology}</span>
+              </div>
 
-                <div className="description">
-                    <p>
-                        {project.description}
-                    </p>
-                </div>
+              <div className={styles.description}>
+                <p>{project.description}</p>
+              </div>
             </div>
-            
-            <button>
-                VISIT
-            </button>
 
-        </div>
-        : 
+            <button className={styles.visitBtn}>VISIT</button>
+          </div>
+        </>
+      ) : (
         <></>
-        }
-        <style jsx>{`
-            .container{
-                height: 100vh;
-                background-color: var(--dark-color);
-                padding-top: 5em;
-                display: flex;
-                flex-direction: column;
-                align-items: strech;
-                //padding: 5em 3em 0 3em;
-            }    
-            .backArrow{
-                position: absolute;
-                top: 1em;
-                left: 1em;
-            }
-            .banner{
-                align-self: center;
-            }
-            .content{
-                width: calc(100% - 6em);
-                align-self: center;
-            }
-            .title{
-                color: var(--light-color);
-                font-family: 'MS Reference Sans Serif';
-                text-align: center;
-                flex-grow: 100%;
-                border-bottom: 1px solid white;
-            }
-            .info{
-                color: var(--light-color);
-                display: grid;
-                grid-template-columns: 1fr 1fr;
-                padding: 1em 0px;
-                row-gap: 1em;
-                white-space: nowrap;
-                font-size: .8em;
-            }
-            .info-value{
-                text-align: right;
-            }
-            p{
-                color: var(--light-color);
-                padding: 1em;
-                border: 1px solid var(--light-color);
-                font-size: .8em;
-            }
-            button{
-                background-color: var(--light-green);
-                padding: 1em;
-                border: none;
-                outline: none;
-                color: floralwhite;
-                border-radius: .3em;
-                width: 30vw;
-                align-self: center;
-                font-size: 1em;
-                font-weight: bold;
-                cursor: pointer;
-                margin-top: 1em;
-            }
-        `}</style>
-    </>)
+      )}
+    </>
+  );
 }
