0922ed540757480390ea3720706f9168c76b46b3
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2021-07-16 10:41:25 +0600

    skills data relocated

---
+++ data/skills.js
@@ -0,0 +1,84 @@
+export const languages = [
+  {
+    name: "Javascript",
+    src: "/images/javascript.jpg",
+    level: "Advance",
+  },
+  {
+    name: "TypeScript",
+    src: "/images/typescript.png",
+    level: "Intermediate",
+  },
+  {
+    name: "Python",
+    src: "/images/python.png",
+    level: "Advance",
+  },
+  {
+    name: "C++",
+    src: "/images/cplusplus.png",
+    level: "Advance",
+  },
+  {
+    name: "C",
+    src: "/images/c.png",
+    level: "Intermediate",
+  },
+  {
+    name: "Java",
+    src: "/images/java.png",
+    level: "Intermediate",
+  },
+];
+
+export const frameworks = [
+  {
+    name: "ReactJs",
+    src: "/images/reactjs.png",
+    level: "Advance",
+    rotate: true,
+  },
+  {
+    name: "Nextjs",
+    src: "/images/nextjs.png",
+    level: "Advance",
+  },
+  {
+    name: "Bootstrap",
+    src: "/images/bootstrap.png",
+    level: "Advance",
+  },
+  {
+    name: "Tailwind css",
+    src: "/images/tailwindcss.png",
+    level: "Advance",
+  },
+  {
+    name: "Django",
+    src: "/images/django.png",
+    level: "Intermediate",
+  },
+];
+
+export const others = [
+  {
+    name: "Git",
+    src: "/images/git.png",
+    level: "Intermediate",
+  },
+  {
+    name: "Sass",
+    src: "/images/sass.png",
+    level: "Intermediate",
+  },
+  {
+    name: "Firebase",
+    src: "/images/firebase.png",
+    level: "Basics",
+  },
+  {
+    name: "Adobe Xd",
+    src: "/images/xd.png",
+    level: "Intermediate",
+  },
+];
