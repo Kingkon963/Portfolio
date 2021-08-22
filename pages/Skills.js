d57b80be4d7e15f672f53a58e7bd821e74a99046
Muhammad Naimul Islam <naimulislam19149@gmail.com>
2021-08-22 21:34:36 +0600

    upwork logo added

--- pages/Skills.js
+++ pages/Skills.js
@@ -1,37 +1,69 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
 import ListSection from "../components/ListSection";
 import SkillIndicator from "../components/SkillIndicator";
 import Container from "../components/Container";
 import { languages, frameworks, others } from "../data/skills";
+import { displayAt } from "../utils/screenSize";
+import styles from "../styles/Skills.module.scss";
+
 export default function Skills() {
   return (
     <>
       <Layout setActiveLink="Skills">
         <Container>
-          <ul>
-            <SkillIndicator level="Advance" />
-            <li>Advance</li>
+          <div className={styles.flex}>
+            <div className={styles.left}>
+              <ul>
+                <SkillIndicator level="Advance" />
+                <li>Advance</li>
 
-            <SkillIndicator level="Intermediate" />
-            <li>Intermediate</li>
+                <SkillIndicator level="Intermediate" />
+                <li>Intermediate</li>
 
-            <SkillIndicator level="Basics" />
-            <li>Basics</li>
-          </ul>
+                <SkillIndicator level="Basics" />
+                <li>Basics</li>
+              </ul>
 
-          <div className="listContainer no-scrollbar">
-            <div>
-              <ListSection title="Languages" elements={languages} size={45} />
+              <div className="listContainer no-scrollbar">
+                <div>
+                  <ListSection
+                    title="Languages"
+                    elements={languages}
+                    size={45}
+                  />
+                </div>
+                <div>
+                  <ListSection
+                    title="Libraries & Frameworks"
+                    elements={frameworks}
+                    size={45}
+                  />
+                </div>
+                <div>
+                  <ListSection title="Others" elements={others} size={45} />
+                </div>
+              </div>
             </div>
-            <div>
-              <ListSection
-                title="Libraries & Frameworks"
-                elements={frameworks}
-                size={45}
-              />
-            </div>
-            <div>
-              <ListSection title="Others" elements={others} size={45} />
+            <div className={styles.right}>
+              {displayAt(
+                ["lg", "xl", "xxl"],
+
+                <>
+                  <h1 className={styles.hireMe}>Hire Me @</h1>
+                  <a
+                    href="https://www.upwork.com/freelancers/~014046cecba2ff7614"
+                    target="_blank"
+                  >
+                    <Image
+                      src="/images/upwork-fill.jpg"
+                      width="700"
+                      height="250"
+                      layout="intrinsic"
+                    />
+                  </a>
+                </>
+              )}
             </div>
           </div>
         </Container>
